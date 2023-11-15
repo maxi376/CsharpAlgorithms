6fa4e9c6c971e12fda959df623226cce01a80e86
maxi333 <max.timofte@example.com>
2023-11-15 08:29:23 +0200

    com0

---
+++ exercises/TimCorneyFullCourseCsharpBeginner/myNamespace/SumDigits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimCorneyFullCourseCsharpBeginner.myNamespace
+{
+    internal class SumDigits
+    {
+        public static void sumDigits()
+        {
+            int n, sum = 0, m;
+            Console.Write("Enter a number: ");
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write("Sum is= " + sum);
+        }
+    }
+}
